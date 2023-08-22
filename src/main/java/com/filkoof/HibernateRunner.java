a816548151cf2b5c6a8bd4a56cca2998d0555051
filkoof <filkof@mail.ru>
2023-08-22 14:27:09 +0300

    Create and save Entity

--- src/main/java/com/filkoof/HibernateRunner.java
+++ src/main/java/com/filkoof/HibernateRunner.java
@@ -1,18 +1,35 @@
 package com.filkoof;
 
+import com.filkoof.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class HibernateRunner {
 
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("petya@gmail.com")
+                    .firstname("Petya")
+                    .lastname("Petrov")
+                    .birthDate(LocalDate.of(2000,1,1))
+                    .age(23)
+                    .build();
+
+            session.save(user);
+
+            session.getTransaction().commit();
         }
     }
 }
